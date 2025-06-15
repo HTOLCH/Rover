c4c4d132bdea4d89459911385c3a68b65a77afc3
Harry <harrytolcher1@gmail.com>
2025-06-15 15:22:20 +0800

    imu working 2

--- src/build/libphidget22/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pioneer1/Documents/Rover/src/phidgets_drivers/libphidget22
-# Build directory: /home/pioneer1/Documents/Rover/src/build/libphidget22
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/build/phidgets_msgs/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(phidgets_msgs_IDL_FILES "srv/SetAnalogOutput.idl;srv/SetDigitalOutput.idl;srv/Trigger.idl;msg/EncoderDecimatedSpeed.idl;msg/StepperCommand.idl;msg/StepperConfig.idl;msg/StepperState.idl")
-set(phidgets_msgs_INTERFACE_FILES "srv/SetAnalogOutput.srv;srv/SetDigitalOutput.srv;srv/Trigger.srv;msg/EncoderDecimatedSpeed.msg;msg/StepperCommand.msg;msg/StepperConfig.msg;msg/StepperState.msg")
